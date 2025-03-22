a2c29f7330bdf704416e6caab250713dcd2dd60a
Emil Maharramov <145191264+emaharramov@users.noreply.github.com>
2025-03-22 20:54:17 +0400

    features

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
@@ -32,7 +36,7 @@ module.exports = async (req, res) => {
       cancel_url: "myapp://cancel",
     });
 
-    res.json({ url: session.url });
+    res.status(200).json({ url: session.url });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
